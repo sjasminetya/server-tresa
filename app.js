9e0ccd8f367f9b4a1147ed8e37ecf1911a671677
shaula <shaulajasminetya22@gmail.com>
2021-12-20 00:38:40 +0700

    Add CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 
 // import mongoose
 const mongoose = require('mongoose');
@@ -46,6 +47,7 @@ app.use(session({
   cookie: { maxAge: 60000 }
 }));
 app.use(flash());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
